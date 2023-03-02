9c67b6e32b24c03cc55c5e8e6e619732190c41f8
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-03-03 01:59:32 +0200

    styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,50 @@
-import { CardBlanc } from "./App.styled";
+import { useState } from "react";
 import {Container}  from "./commonStyles/Common.styled"
+import { CardBlanc, ButtonFollow, Content, DescriptionText  } from "./App.styled";
+
 
 export const App = () => {
+  const [followersCount, setFollowersCount] = useState(100500);
+  const [followingStatus, setFollowingStatus] = useState('FOLLOW');
+  const [isFollowing, setIsFollow] = useState(false);
 
+  const toggleFollowingStatus = () => {
+    if (isFollowing === false) {
+      setIsFollow(true);
+      setFollowingStatus('FOLLOWING');
+      setFollowersCount(prev => prev + 1);
+    } else if (isFollowing === true) {
+      setIsFollow(false);
+      setFollowingStatus('FOLLOW');
+      setFollowersCount(prev => prev - 1);
+    }
+  };
+
+  const formatCount = number => {
+    const numberFormat = new Intl.NumberFormat('en-US');
+    return numberFormat.format(number);
+  };
 
   return (
     <main>
       <Container>
         <CardBlanc>
-          <div>
+          <Content>
             <div>
             line avatar
             </div>
             <div>
-              tweets
-              follo
+              <DescriptionText> 777 TWEETS</DescriptionText>
+              <DescriptionText> {formatCount(followersCount)} FOLLOWERS</DescriptionText>
             </div>
-            <button>
-
-            </button>
-          </div>
+            <ButtonFollow
+              type="button"
+              style={isFollowing ? { backgroundColor: "#5CD3A8" } : {}}
+              onClick={toggleFollowingStatus}
+            >
+              {followingStatus}
+            </ButtonFollow>
+          </Content>
         </CardBlanc>  
       </Container>
     </main>
--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,18 +1,62 @@
 import styled from 'styled-components';
+import decor_picture from "../images/decor_picture.png";
+import logo from "../images/logo.png";
+import round from "../images/round.png";
 
-// export const Container = styled.div`
-//     height: 100vh;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
- 
-// `;
 
 export const CardBlanc = styled.div`
     position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
     width: 380px;
     height: 460px;
     border-radius: 20px;
-    background: linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%);
+    background: url("${logo}"), url("${decor_picture}"),
+    linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%);
     box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+    background-size: auto, auto, cover;
+    background-repeat: no-repeat, no-repeat, no-repeat;
+    background-position: top 20px left 20px, top 28px left 36px, center;
+`;
+
+export const Content = styled.div`
+  padding-bottom: 36px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 26px;
+  height: 238px;
+  > div p:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+
+
+export const DescriptionText = styled.p`
+    font-family: 'Montserrat';
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 1.2;
+    color: #EBD8FF;
+    text-align: center;
+`;
+
+export const ButtonFollow = styled.button`
+    width: 196px;
+    height: 50px;
+    border: none;
+    background: #EBD8FF;
+    box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+    border-radius: 10.31px;
+    cursor: pointer;
+    color: #373737;
+    font-size: 18px;
+    line-height: 1.37;
+    line-height: 1.22;
+    font-weight: 600;
+    font-family: 'Montserrat';  
 `;

dc61da61ddd57f5854de43ed4a5ae41edb1c75d4
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-03-08 17:10:57 +0200

    localStor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {Container } from "./commonStyles/Common.styled"
 import {CardBlanc} from "./App.styled";
 import { TextInfo } from "./TextInfo/TextInfo";
@@ -6,9 +6,27 @@ import { LineAvatar } from "./LineAvatar/LineAvatar";
 import { Button }from "./Button/Button";
 
 export const App = () => {
-  const [followersCount, setFollowersCount] = useState(100500);
-  const [followingStatus, setFollowingStatus] = useState('FOLLOW');
-  const [isFollowing, setIsFollow] = useState(false);
+  const storedSettings = JSON.parse(localStorage.getItem('settings'));
+
+  const [followersCount, setFollowersCount] = useState(() =>
+    storedSettings ? storedSettings.followersCount : 100500
+  );
+  const [followingStatus, setFollowingStatus] = useState(() =>
+    storedSettings ? storedSettings.followingStatus : 'FOLLOW'
+  );
+  const [isFollowing, setIsFollow] = useState(() =>
+    storedSettings ? storedSettings.isFollowing : false
+  );
+
+  useEffect(() => {
+    const settings = {
+      followersCount,
+      followingStatus,
+      isFollowing,
+    };
+
+    localStorage.setItem('settings', JSON.stringify(settings));
+  }, [isFollowing, followersCount, followingStatus]);
 
   const toggleFollowingStatus = () => {
     if (isFollowing === false) {
